efa4e8d6578babf150f8b6c0b0e4a2e32ce2a627
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-11-21 03:04:52 +0800

    update

--- jupyter_server_app/boot.sh
+++ jupyter_server_app/boot.sh
@@ -2,7 +2,21 @@
 docker info
 
 # 2. 构建镜像
-docker build -t jupyter_server_app .
+docker build -t jupyter_server_image .
 
-# 3. 运行容器
-docker run -d -p 8888:8888 -v $(pwd)/jupyter_workspace:/workspace jupyter_server_app
+# 3. 测试容器
+docker run \
+    --rm \
+    --gpus all \
+    -it \
+    --name jupyter_server_app \
+    jupyter_server_image /bin/bash
+
+# 4. 运行容器
+docker run \
+    --gpus all \
+    --name jupyter_server_app \
+    -d \
+    -p 9999:8888 \
+    -v $(pwd)/jupyter_workspace:/workspace \
+    jupyter_server_image
